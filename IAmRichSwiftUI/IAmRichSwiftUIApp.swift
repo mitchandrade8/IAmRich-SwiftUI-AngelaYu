0527a6f7b75403f2d8f5b61fc32c16d611387da5
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-02-23 10:52:54 -0700

    Initial Commit

---
+++ IAmRichSwiftUI/IAmRichSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichSwiftUIApp.swift
+//  IAmRichSwiftUI
+//
+//  Created by Mitch Andrade on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
